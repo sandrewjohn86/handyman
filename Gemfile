ec51e7eec0301b9f56cb2972ced23ad7b2e14500
Stephen John <you@example.com>
2017-11-26 21:16:30 -0400

    Added Rails Admin gem and custom field to users

--- Gemfile
+++ Gemfile
@@ -12,6 +12,7 @@ gem 'rails', '~> 5.0.6'
 # gem 'sqlite3'
 gem 'mysql2'
 gem 'devise'
+gem 'rails_admin', '~> 1.2'
 # Use Puma as the app server
 gem 'puma', '~> 3.0'
 # Use SCSS for stylesheets
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171126093924) do
+ActiveRecord::Schema.define(version: 20171126105623) do
 
   create_table "admins", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
     t.string   "email",                  default: "", null: false
@@ -107,18 +107,18 @@ ActiveRecord::Schema.define(version: 20171126093924) do
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
-    t.string   "email",                                default: "", null: false
-    t.string   "encrypted_password",                   default: "", null: false
+    t.string   "email",                                default: "",    null: false
+    t.string   "encrypted_password",                   default: "",    null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",                        default: 0,  null: false
+    t.integer  "sign_in_count",                        default: 0,     null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                                        null: false
-    t.datetime "updated_at",                                        null: false
+    t.datetime "created_at",                                           null: false
+    t.datetime "updated_at",                                           null: false
     t.string   "fname"
     t.string   "lname"
     t.string   "phone"
@@ -126,6 +126,7 @@ ActiveRecord::Schema.define(version: 20171126093924) do
     t.integer  "rate"
     t.text     "intro_summary",          limit: 65535
     t.integer  "skill_id"
+    t.boolean  "admin",                                default: false
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
